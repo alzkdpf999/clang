1d384af36d0852b9a22869bbb8086eb57091fcd9
alzkdpf999 <alzkdpf999@naver.com>
2022-07-14 12:07:03 +0900

    7.14

---
+++ 7.14/7.14/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+	//변수명 이름 규칙 : 영문자 대소문자 및 숫자 및 언더바(_)만가능
+	//숫자는 첫글자로 올 수 없음 
+	//띄어쓰기 불가
+	int a = 3; //변수 선언
+	printf("%d , %d\n ",5,5);
+	float number = 15.4;
+	printf("%.2f", number);
+}
